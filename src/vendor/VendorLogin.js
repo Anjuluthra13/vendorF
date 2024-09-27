539f67ed64c002979e10c7503697c97c579a8696
Anjuluthra13 <122430165+Anjuluthra13@users.noreply.github.com>
2024-09-27 16:46:21 +0530

    deploy

--- src/vendor/VendorLogin.js
+++ src/vendor/VendorLogin.js
@@ -12,7 +12,7 @@ const Login = () => {
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:5000/api/login', { email, password });
+            const response = await axios.post('vendorb-production.up.railway.app/api/login', { email, password });
     
             // If login is successful
             if (response.status === 200) {
